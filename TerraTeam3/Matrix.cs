845f08999c5eee9f940914fc7fe8aa81b80a2d05
Romain Bancken <rbford@proximus.be>
2018-05-24 13:20:40 +0200

    dag 3

--- TerraTeam3/Matrix.cs
+++ TerraTeam3/Matrix.cs
@@ -11,7 +11,8 @@ namespace TerraTeam3
         private int aantalRijen;
         private int aantalKolommen;
         private int aantalPosities;
-        public List<MatrixItem> Items; 
+        public List<MatrixItem> Items = new List<MatrixItem>();
+        Random rnd = new Random();
 
         public Matrix(int aantalRijen, int aantalKolommen)
         {
@@ -25,33 +26,52 @@ namespace TerraTeam3
                     LeegItem leegItem = new LeegItem();
                     leegItem.PosX = x;
                     leegItem.PosY = y;
-                    leegItem.Naam = "."+"_" + x +"_"+ y;
+                    leegItem.Naam = "." + "_" + x + "_" + y;
                     VulMatrixToe(leegItem);
+
                 }
             }
         }
 
         public void VoegItemToe(MatrixItem matrixItem)
         {
-            List<MatrixItem> leegItems =( from item in Items
-                            where item.Symbool == '.'
-                            select item).ToList();
+            List<MatrixItem> leegItems = (from item in Items
+                                          where item.Symbool == '.'
+                                          select item).ToList();
 
-            Random rnd = new Random();
-
-            var randomGeselecteerdItem = leegItems[rnd.Next(leegItems.Count())];
+            var randomGeselecteerdItem = leegItems[rnd.Next(0, leegItems.Count())];
 
             matrixItem.PosX = randomGeselecteerdItem.PosX;
             matrixItem.PosY = randomGeselecteerdItem.PosY;
 
             Items.Remove(randomGeselecteerdItem);
             Items.Add(matrixItem);
-
         }
 
         public void VulMatrixToe(MatrixItem matrixItem)
         {
             Items.Add(matrixItem);
+        }
+
+        public void GeefWeer()
+        {
+            List<MatrixItem> weerTeGevenItems = (from item in Items
+                                                 orderby item.PosX, item.PosY
+                                                 select item).ToList();
+
+            var kolomTeller = 0;
+
+            foreach (var item in weerTeGevenItems)
+            {
+                kolomTeller++;
+
+                Console.Write(item.Symbool);
+                if (kolomTeller == aantalKolommen)
+                {
+                    Console.WriteLine();
+                    kolomTeller = 0;
+                }
+            }
         }
     }
 }
