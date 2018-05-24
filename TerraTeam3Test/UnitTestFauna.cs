758b3db6ab824c9cfe9f92a2a7d3c9af581983c7
Jacky Hermans <hermans_stevens@skynet.be>
2018-05-24 09:14:36 +0200

    UnitTests gecreëerd - nog leeg

---
+++ TerraTeam3Test/UnitTestFauna.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TerraTeam3Test
+{
+    [TestClass]
+    public class UnitTestFauna
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TerraTeam3Test/UnitTestLeegItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TerraTeam3Test
+{
+    [TestClass]
+    public class UnitTestLeegItem
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
